abb6740728b6239d20c1d723be1ce83cdf2913a6
Nutty4616 <nkosin361@gmail.com>
2023-04-28 21:43:44 +0200

    Making a event listener on method

--- nathiLib.test.js
+++ nathiLib.test.js
@@ -15,4 +15,27 @@ describe("NathiLib", () => {
     nathiLib.addClass(element, "test-class");
     expect(element.classList.contains("test-class")).toBe(true);
   });
+
+  test("should add an event listener to the specified element", () => {
+    const element = document.createElement("button");
+    const mockCallback = jest.fn();
+
+    customLib.on(element, "click", mockCallback);
+    element.click();
+
+    expect(mockCallback).toHaveBeenCalled();
+  });
+
+  test("should add an event listener using a CSS selector string", () => {
+    document.body.innerHTML = `
+      <button id='test-button'></button>
+    `;
+
+    const mockCallback = jest.fn();
+    nathiLib.on("#test-button", "click", mockCallback);
+    const element = document.querySelector("#test-button");
+    element.click();
+
+    expect(mockCallback).toHaveBeenCalled();
+  });
 });
